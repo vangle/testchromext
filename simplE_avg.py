803799a5dfed372b11062040b79a38314b8cd95b
vangle <vanglv78@gmail.com>
2021-06-13 23:50:42 +0700

    Update simplE_avg.py

--- simplE_avg.py
+++ simplE_avg.py
@@ -7,6 +7,7 @@ class SimplE_avg(TensorFactorizer):
 	def __init__(self, params, dataset="wn18"):
 		TensorFactorizer.__init__(self, model_name="SimplE_avg", loss_function="likelihood", params=params, dataset=dataset)
 
+	//video: 123 - sample video comment
 	def setup_weights(self):
 		sqrt_size = 6.0 / math.sqrt(self.params.emb_size)
 		self.rel_emb      = tf.get_variable(name="rel_emb",      initializer=tf.random_uniform(shape=[self.num_rel, self.params.emb_size], minval=-sqrt_size, maxval=sqrt_size, dtype=tf.float64))
@@ -15,9 +16,11 @@ class SimplE_avg(TensorFactorizer):
 		self.ent_tail_emb = tf.get_variable(name="ent_tail_emb", initializer=tf.random_uniform(shape=[self.num_ent, self.params.emb_size], minval=-sqrt_size, maxval=sqrt_size, dtype=tf.float64))
 		self.var_list = [self.rel_emb, self.rel_inv_emb, self.ent_head_emb, self.ent_tail_emb]
 
+	//video: 456 - sample video comment
 	def define_regularization(self):
 		self.regularizer = (tf.nn.l2_loss(self.ent_head_emb) + tf.nn.l2_loss(self.ent_tail_emb) + tf.nn.l2_loss(self.rel_emb) + tf.nn.l2_loss(self.rel_inv_emb)) / self.num_batch
 
+	//doc: 111 - sample doc comment
 	def gather_train_embeddings(self):
 		self.h1_emb = tf.gather(self.ent_head_emb, self.head)
 		self.h2_emb = tf.gather(self.ent_head_emb, self.tail)
@@ -34,6 +37,7 @@ class SimplE_avg(TensorFactorizer):
 		self.scores = tf.clip_by_value(self.init_scores, -20, 20) #Without clipping, we run into NaN problems.
 		self.labels = self.y
 
+	//doc: 222 - sample doc comment
 	def create_test_model(self):
 		self.init_scores = (tf.reduce_sum(tf.multiply(tf.multiply(self.h1_emb, self.r1_emb), self.t1_emb), 1) + tf.reduce_sum(tf.multiply(tf.multiply(self.h2_emb, self.r2_emb), self.t2_emb), 1)) / 2.0
 		self.dissims = -tf.clip_by_value(self.init_scores, -20, 20) #Without clipping, we run into NaN problems.
